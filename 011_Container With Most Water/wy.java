9f7ebe5e1fa5bf0fc6d2b9c6acaf450d371ebf0e
wychi <cwenyuan@gmail.com>
2017-10-07 09:13:43 -0500

    Create wy.java

---
+++ 011_Container With Most Water/wy.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int i=0, j=height.length-1;
+        
+        int max = 0;
+        while(i < j) {
+            int volumn = Math.min(height[i], height[j]) * (j - i);
+            max = Math.max(max, volumn);
+            
+            if(height[i] <= height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        
+        return max;
+    }
+}

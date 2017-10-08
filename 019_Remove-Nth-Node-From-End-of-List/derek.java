6e6d198e2a55e44ff8dcbecedff9c85fb8647cb0
derek.lai <derek.lai@ileopard.com>
2017-10-08 23:03:06 +0800

    Update 19

---
+++ 019_Remove-Nth-Node-From-End-of-List/derek.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    //Key: Keep gap 'N' fast pointer between slow pointer
+    //Question: if head is null, what is expect answer ? null ?
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (null == head || n < 0) {
+            return null;
+        }
+        ListNode start = new ListNode(0);
+        ListNode slow = start;
+        ListNode fast = start;
+        start.next = head;
+        
+        for (int i = 0; i<=n; i++) {
+            if (null != fast) {
+                fast = fast.next;
+            }
+        }
+        
+        while (null != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        
+        if (null != slow.next) {
+            slow.next = slow.next.next;
+        }
+        
+        return start.next;
+    }
+}

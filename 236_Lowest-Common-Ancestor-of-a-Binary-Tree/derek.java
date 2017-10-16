76a1dc8d5d499c9a841d1b1681470ecb686d4a03
derek.lai <derek.lai@ileopard.com>
2017-10-16 17:56:09 +0800

    Udpate 236

---
+++ 236_Lowest-Common-Ancestor-of-a-Binary-Tree/derek.java
@@ -0,0 +1,11 @@
+//Key:
+    //1. Confirm is BST or not !!!
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (null == root || null == p || null == q
+            || root == q || root == p) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (null != left && null != right) return root;
+        return null != left ? left : right;
+    }
+    

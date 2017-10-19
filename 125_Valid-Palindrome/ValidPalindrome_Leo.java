71f1d0705ff557c295ee7af07780c7a9e7d6e63a
leareai <leareai@users.noreply.github.com>
2017-10-19 03:15:44 -0500

    Update ValidPalindrome_Leo.java

--- 125_Valid-Palindrome/ValidPalindrome_Leo.java
+++ 125_Valid-Palindrome/ValidPalindrome_Leo.java
@@ -1,24 +1,23 @@
-public class Solution {
-  public boolean isPalindrome(String s) {
-    int i = 0;
-    int j = s.length() - 1;
-    int toLower = 'a' - 'A';
-    while (i < j) {
-      char left = s.charAt(i);
-      
-      if ('A' <= left && left <= 'Z') {
-        left += toLower;
-      } else if (!(('a' <= left && left <= 'z') || ('0' <= left && left <= '9'))) {
-          i++;
-          continue;
-      }
-      char right = s.charAt(j--);
-      if ('A' <= right && right <= 'Z') {
-        right += toLower;
-      } else if (!(('a' <= right && right <= 'z') || ('0' <= right && right <= '9'))) continue;
-      if (left != right) return false;
-      i++;    
+class Solution {
+    private static char OFFSET = 'a' - 'A';
+    private char getChar(String s, int idx) {
+        char c = s.charAt(idx);
+        if (('A' <= c && c <= 'Z')) return (char)(c + OFFSET);
+        return ('a' <= c && c <= 'z') || ('0' <= c && c <= '9') ? c : 0;
     }
-    return true;
-  }
+    public boolean isPalindrome(String s) {
+        int l = 0, h = s.length() - 1;
+        while (l < h) {
+            char cl = getChar(s, l), cr;
+            if (cl == 0) {
+                l++;
+            } else if ((cr = getChar(s, h)) == 0) {
+                h--;
+            } else {
+                if (cl != cr) return false;
+                l++;h--;
+            }
+        }
+        return true;
+    }
 }

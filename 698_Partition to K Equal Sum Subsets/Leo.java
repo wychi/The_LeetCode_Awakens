552bf1d8d944a61effc20365b027724eb36aff66
leareai <leareai@users.noreply.github.com>
2017-10-19 05:17:01 -0500

    Create Leo.java

---
+++ 698_Partition to K Equal Sum Subsets/Leo.java
@@ -0,0 +1,22 @@
+class Solution {
+    private boolean canPartition(int[] nums, boolean[] choose, int k, int start, int current, int target) {
+        if (1 == k) return true;
+        if (current > target) return false;
+        if (target == current) return canPartition(nums, choose, k-1, 0, 0, target);
+        for (int i = start; i < nums.length; i++) {
+            if (choose[i]) continue;
+            choose[i] = true;
+            if (canPartition(nums, choose, k, i+1, current+nums[i], target)) return true;
+            choose[i] = false;
+        }
+        return false;
+    }
+    
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        int sum = 0;
+        for (int v:nums) sum+=v;
+        if (sum%k != 0) return false;
+        boolean[] choose = new boolean[nums.length];
+        return canPartition(nums, choose, k, 0, 0, sum/k);
+    }
+}

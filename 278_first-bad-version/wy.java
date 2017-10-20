c002571049f02f4d7d4944a732502904b70256f7
wychi <cwenyuan@gmail.com>
2017-10-19 23:16:34 -0500

    Create wy.java

---
+++ 278_first-bad-version/wy.java
@@ -0,0 +1,21 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int i = 1;
+        int j = n;
+        while(i<=j) {
+            int mid = i + (j-i)/2;
+            if(isBadVersion(mid)) {
+                if(!isBadVersion(mid - 1))
+                    return mid;
+                
+                j = mid - 1;
+            } else {
+                i = mid + 1;
+            }
+        }
+        return 1;
+    }
+}

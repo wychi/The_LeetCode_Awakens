a242502b5d71221f57ffbe2b862f4720b4708ab2
leareai <leareai@users.noreply.github.com>
2017-10-25 02:50:01 +0800

    Update Leo.java

--- 347_Top K Frequent Elements/Leo.java
+++ 347_Top K Frequent Elements/Leo.java
@@ -34,4 +34,29 @@ class Solution {
         }
         return ret;
     }
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int n : nums) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+        
+        LinkedList<Integer>[] freqLists = new LinkedList[nums.length + 1];
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            Integer freq = entry.getValue();
+            if (freqLists[freq] == null) freqLists[freq] = new LinkedList<>();
+            freqLists[freq].add(entry.getKey());
+        }
+
+        List<Integer> ret = new ArrayList<>();
+        outer:
+        for (int i = freqLists.length - 1; i >= 0; i--) {
+            if (freqLists[i] != null) {
+                for (Integer v: freqLists[i])  {
+                    ret.add(v);
+                    if (--k <= 0) break outer;
+                }
+            }
+        }
+        return ret;
+    }
 }

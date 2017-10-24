3d6136806c6bba266c2ddc5da9bc910ca0287cc4
derek.lai <derek.lai@ileopard.com>
2017-10-25 00:01:00 +0800

    add 525

---
+++ 525_Contiguous Array/derek.java
@@ -0,0 +1,26 @@
+class Solution {
+    //Time:  O(N)
+    //Space: O(N)
+    public int findMaxLength(int[] nums) {
+        final int size = null != nums ? nums.length : 0;
+        if (size <= 0) return 0;
+        int zeroCnt = 0, oneCnt = 0;
+        int max = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        for (int i = 0 ; i<size; i++) {
+            if (nums[i] == 0) {
+                zeroCnt++;
+            } else {
+                oneCnt++;
+            }
+            
+            if (map.containsKey(zeroCnt-oneCnt)) {
+                max = Math.max(max, i-map.get(zeroCnt-oneCnt));
+            } else {
+                map.put((zeroCnt-oneCnt), i);
+            }
+        }
+        return max;
+    }
+}

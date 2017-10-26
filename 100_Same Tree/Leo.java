ec025e0ee2f25dbf2a2464e9141b66aaea79f166
leareai <leareai@users.noreply.github.com>
2017-10-27 03:37:24 +0800

    Update Leo.java

--- 100_Same Tree/Leo.java
+++ 100_Same Tree/Leo.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null) return (q == null);
+        if (q == null) return false;
         return p.val == q.val && isSameTree(p.left , q.left) && isSameTree(p.right, q.right);
     }
 }

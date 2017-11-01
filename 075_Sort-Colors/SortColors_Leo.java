f6c39f4bac07f189f5ebecde2b16e931259176b8
leareai <leareai@users.noreply.github.com>
2017-11-02 01:42:15 +0800

    Update SortColors_Leo.java

--- 075_Sort-Colors/SortColors_Leo.java
+++ 075_Sort-Colors/SortColors_Leo.java
@@ -7,19 +7,26 @@
 public class Solution {
   private static final int COLOR_NUMBER = 3;
   public void sortColors(int[] nums) {
-    int[] colorSet = new int[COLOR_NUMBER];
-    for (int v : nums) {
-      colorSet[v]++;
-    }
-   
-    int startIndex = 0;
-    int number = 0;
-    for (int i = 0; i < COLOR_NUMBER; i++) {
-      number += colorSet[i];
-      for (int j = startIndex; j < number; j++) {
-        nums[j] = i;
-      }
-      startIndex = number;
+        int[] counts = new int[COLOR_NUMBER];
+        for (int v: nums) counts[v]++;
+        for (int color = 0, idx = 0; color < COLOR_NUMBER; color++) {
+            int c = counts[color];
+            while (c-->0) nums[idx++] = color;
+        }
     }
   }
+  public void sortColors(int[] nums) {
+        int i = 0, l = 0, h = nums.length-1; //2,1,2
+        while (i <= h) {
+            if (nums[i] == 0)  {
+                nums[i++] = nums[l];
+                nums[l++] = 0;
+            } else if (nums[i] == 2) {
+                nums[i] = nums[h];
+                nums[h--] = 2;
+            } else {
+                i++;
+            }
+        }
+    }
 }

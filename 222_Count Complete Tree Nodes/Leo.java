c039209c635fa89edc939b4de3f451b57578ab9f
leareai <leareai@users.noreply.github.com>
2017-11-10 04:26:19 +0800

    Update Leo.java

--- 222_Count Complete Tree Nodes/Leo.java
+++ 222_Count Complete Tree Nodes/Leo.java
@@ -11,4 +11,21 @@ class Solution {
         return (1<<h) + ((left == null) ? countNodes(root.right) : 
             ( countNodes(root.left)));
     }
+    
+    public int countNodes(TreeNode root) {
+        if (root == null) return 0;
+        int ret = 0; 
+        while (null != root) {
+            int h = 0;
+            TreeNode left = root.left, right = root.right;
+            while (null != right) {
+                left = left.left;
+                right = right.left;
+                h++;
+            }
+            ret += (1<<h);
+            root = (null == left) ? root.right : root.left;
+        }
+        return ret;
+    }
 }

8e27cb8b3bbea82860312489c019c2de5d5d4dcf
leareai <leareai@users.noreply.github.com>
2017-11-13 01:04:21 +0800

    Update Leo.java

--- 623_Add One Row to Tree/Leo.java
+++ 623_Add One Row to Tree/Leo.java
@@ -19,4 +19,28 @@ class Solution {
         doAddOneRow(dummy, v, d);
         return dummy.left;
     }
+    
+    public TreeNode addOneRow(TreeNode root, int v, int d) {
+        TreeNode dummy = new TreeNode(0);
+        dummy.left = root;
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.push(dummy); 
+        while (d-- > 1) {
+            for (int i = 0, size = queue.size(); i < size; i++) {
+                TreeNode n = queue.pop();
+                if (n.left != null) queue.add(n.left);
+                if (n.right != null) queue.add(n.right);
+            }
+        }
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.pop();
+            TreeNode n = new TreeNode(v);
+            n.left = node.left;
+            node.left = n;
+            n = new TreeNode(v);
+            n.right = node.right;
+            node.right = n;
+        }
+        return dummy.left;
+    }
 }

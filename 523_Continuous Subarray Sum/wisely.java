b2fcbf82102758f4f532fef9f57fee45393f0c5c
wisely.hsu <wisely.hsu@ileopard.com>
2017-11-17 10:23:44 +0800

    commit 523 o(n) solution

--- 523_Continuous Subarray Sum/wisely.java
+++ 523_Continuous Subarray Sum/wisely.java
@@ -1,8 +1,24 @@
-
 class Solution {
-
-    // O(n^2)
     public boolean checkSubarraySum(int[] nums, int k) {
+        if (k<0) k=Math.abs(k);
+        int sum=0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i=0; i<nums.length; i++) {
+            sum+=nums[i];
+            if (k!=0) sum=sum%k;
+            if (k==0&&sum!=0) return false;
+            if (sum==0 && i>0) return true;
+            int res = k-sum;
+            if (map.containsKey(res)) {
+                return true;
+            }
+            map.put(res, i);
+        }
+        return false;
+    }
+    
+    //O(n^2)
+    public boolean checkSubarraySum2(int[] nums, int k) {
         if (k<0) k=Math.abs(k);
         long[] sums=new long[nums.length];
         long sum=0;
